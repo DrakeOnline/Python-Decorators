cb79bee250c454c36eca71b4d7774e9f1ed093f7
DrakeOnline <drakec1997@gmail.com>
2020-10-24 23:16:02 -0700

    Added the counter decorator / Ho aggiunto il decoratore del contatore

--- Main.py
+++ Main.py
@@ -13,5 +13,30 @@
 #   Lo Scrittore:     	Drake G. Cummings                                       		|
 #   Lo Scopo:  	 	Giocare con i decoratori di pitone                        		|
 #-----------------------------------------------------------------------------------------------|
-#   Tempo totale impiegato per la programmazione: 0 Or(a/e) 0 Minut(o/i) 0 Second(o/i) 		|
+#   Tempo totale impiegato per la programmazione: 0 or(a/e) 0 minut(o/i) 0 second(o/i) 		|
 #-----------------------------------------------------------------------------------------------|
+
+
+# (English)  The counter decorator
+# (Italiano) Il decoratore del contatore 
+def CallCounter(func):
+	def Helper(x):
+		Helper.calls += 1
+		return func(x)
+	Helper.calls = 0
+	return Helper
+
+
+# (English)  Decorate the test function with the counter
+# (Italiano) Decora la funzione di test con il contatore
+@CallCounter
+def Tester(x):
+	return x**x
+
+# (English)  Run if this file was ran directly
+# (Italiano) Esegui se questo file è stato eseguito direttamente
+if __name__ == "__main__":
+	print(Tester.calls)
+	for i in range(10):
+		print(Tester(i))
+	print(Tester.calls)
