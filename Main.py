0c93441e616bd7f02ee6c6f55a0a806e6c366b98
DrakeOnline <drakec1997@gmail.com>
2020-10-24 21:33:39 -0700

    Initial Commit / Impegno Iniziale

---
+++ Main.py
@@ -0,0 +1,17 @@
+# (English)
+#-------------------------------------------------------------------------------|
+#   Title:      Decorators                                                      |
+#   Author:     Drake G. Cummings                                               |
+#   Purpose:    Fiddling with the Python decorators                             |
+#-------------------------------------------------------------------------------|
+#   Total time spent programming: 0 hour(s) 0 minute(s) 0 second(s)             |
+#-------------------------------------------------------------------------------|
+#
+# (Italiano)
+#-----------------------------------------------------------------------------------------------|
+#   Il Titolo:  	Decoratori                                              		|
+#   Lo Scrittore:     	Drake G. Cummings                                       		|
+#   Lo Scopo:  	 	Giocare con i decoratori di pitone                        		|
+#-----------------------------------------------------------------------------------------------|
+#   Tempo totale impiegato per la programmazione: 0 Or(a/e) 0 Minut(o/i) 0 Second(o/i) 		|
+#-----------------------------------------------------------------------------------------------|
